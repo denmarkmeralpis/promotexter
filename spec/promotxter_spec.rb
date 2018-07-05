5211af7751b47fd11230c99d6911d1fa7672215f
Cecille Sevilla <cecillegsevilla01@gmail.com>
2018-07-05 13:47:15 +0800

    Added .env sample file

--- spec/promotxter_spec.rb
+++ spec/promotxter_spec.rb
@@ -3,7 +3,22 @@ RSpec.describe Promotxter do
     expect(Promotxter::VERSION).not_to be nil
   end
 
-  it "has an initialize method" do
-  	expect(Promotxter::Client).to respond_to? :initialize
+  context "Client" do
+
+  	context "when initializing a request" do
+    it "should have an API key and an API secret variable" do
+      client = Promotxter::Client.new({apiKey: 'hfdkjsagkjgsajkgjk', apiSecret: 'sampleapikeysdfhfsht', from: 'DEMO'})
+      response = client.send_message({to: '639369642045', text: 'testing'})
+      expect(client.apiKey).to eq "hfdkjsagkjgsajkgjk"
+      expect(client.apiSecret).to eq "sampleapikeysdfhfsht"
+    end
   end
+
+  	it "must be able to send a message" do
+  		client = Promotxter::Client.new()
+  		response = client.send_message({ to: '639175018430', text: 'testing' })
+  		expect(response["status"]).should eq "ok"
+  		expect(response["message"]["to"]).should eq '639175018430'
+  	end
+	end
 end
