d6b3ceb93da069890b1a2e9d523110a1b811cf51
Cecille Sevilla <cecillegsevilla01@gmail.com>
2018-07-05 12:07:58 +0800

    WIP: starting tests

--- spec/promotxter_spec.rb
+++ spec/promotxter_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe Promotxter do
     expect(Promotxter::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "has an initialize method" do
+  	expect(Promotxter::Client).to respond_to? :initialize
   end
 end
