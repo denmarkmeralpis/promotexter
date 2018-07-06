6d0dbc00bbee2e098bf6c5c8ae01d0d9c4cbcdd8
Jett Robin Andres <jettrobin.andres@gmail.com>
2018-07-06 11:12:03 +0800

    Replaced double quotes

--- spec/promotxter_spec.rb
+++ spec/promotxter_spec.rb
@@ -2,22 +2,22 @@ require 'dotenv'
 Dotenv.load
 
 RSpec.describe Promotxter do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Promotxter::VERSION).not_to be nil
   end
 
-  context "when initializing a request" do
-    it "should have an API key" do
+  context 'when initializing a request' do
+    it 'should have an API key' do
       client = Promotxter::Client.new(api_key: ENV['PROMOTXTER_API_KEY'], api_secret: ENV['PROMOTXTER_API_SECRET'], from: ENV['PROMOTXTER_FROM'])
       expect(client.api_key).to eq ENV['PROMOTXTER_API_KEY']
     end
 
-    it "should have an API secret" do
+    it 'should have an API secret' do
       client = Promotxter::Client.new(api_key: ENV['PROMOTXTER_API_KEY'], api_secret: ENV['PROMOTXTER_API_SECRET'], from: ENV['PROMOTXTER_FROM'])
       expect(client.api_secret).to eq ENV['PROMOTXTER_API_SECRET']
     end
 
-    it "should have a sender id" do
+    it 'should have a sender id' do
       client = Promotxter::Client.new(api_key: ENV['PROMOTXTER_API_KEY'], api_secret: ENV['PROMOTXTER_API_SECRET'], from: ENV['PROMOTXTER_FROM'])
       response = client.send_message({to: ENV['RECEIVING_NUMBER'], text: 'testing2'})
       expect(client.from).to eq ENV['PROMOTXTER_FROM']
@@ -25,22 +25,22 @@ RSpec.describe Promotxter do
     end
   end
 
-  context "when sending a message" do
-    it "should have an 'ok' status" do
+  context 'when sending a message' do
+    it 'should have an 'ok' status' do
       client = Promotxter::Client.new(api_key: ENV['PROMOTXTER_API_KEY'], api_secret: ENV['PROMOTXTER_API_SECRET'], from: ENV['PROMOTXTER_FROM'])
       response = client.send_message({to: ENV['RECEIVING_NUMBER'], text: 'testing2'})
       expect(response['status']).to eq 'ok'
     end
   end
 
-  context "when given an invalid sender id" do
-    it "should return an invalid senderId error message" do
+  context 'when given an invalid sender id' do
+    it 'should return an invalid senderId error message' do
       client = Promotxter::Client.new(api_key: ENV['PROMOTXTER_API_KEY'], api_secret: ENV['PROMOTXTER_API_SECRET'], from: 'sender')
       response = client.send_message({to: ENV['RECEIVING_NUMBER'], text: 'testing2'})
       expect(response['message']).to eq 'Invalid SenderID'
     end
 
-    it "should return a 400 status code" do
+    it 'should return a 400 status code' do
       client = Promotxter::Client.new(api_key: ENV['PROMOTXTER_API_KEY'], api_secret: ENV['PROMOTXTER_API_SECRET'], from: 'sender')
       response = client.send_message({to: ENV['RECEIVING_NUMBER'], text: 'testing2'})
       expect(response['statusCode']).to eq 400
